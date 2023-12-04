2be3e8add52af929d51c74681217194e887f4a7a
Alexandre Hermes <133255531+Alexandre200319@users.noreply.github.com>
2023-12-04 19:43:29 -0300

    atualizando botão editar

--- front-end/src/routes/Clientes.jsx
+++ front-end/src/routes/Clientes.jsx
@@ -3,13 +3,14 @@ import ReactDOM from "react-dom";
 import { NavLink } from 'react-router-dom';
 
 
+
 const Clientes = () => {
 
   const defaultValue = []
   const [clientes, setClientes] = useState(defaultValue);
 
   const getApiData = async () => {
-    const response = await fetch('https://cryptic-stream-94767-b9f22ccd744b.herokuapp.com/1',  {method: 'GET'})
+    const response = await fetch('https://localhost:8080/',  {method: 'GET'})
     .then(response => response.json())
     .then(response => {
       setClientes(response.data)
@@ -37,53 +38,56 @@ const Clientes = () => {
         <table className="w-full text-sm text-left rtl:text-right text-white dark:text-white">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-white">
             <tr>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Endereço
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Cliente
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Data Fim
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Valor Cobertura
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Valor Franquia
               </th>
-              <th scope="col" className="px-6 py-3">
+              <th scope="col" className="px-2 py-2">
                 Qtd Sinistro
               </th>
               <th scope="col" className="px-6 py-3">
-                <span className="sr-only">Ações</span>
+                <span className="sr-only">edit</span>
               </th>
             </tr>
           </thead>
           <tbody>
-          {clientes.map((cliente) => (
+          
 
           <tr className="bg-white border-b  dark:bg-gray-800 dark:border-gray-700">
-            <td className="px-6 py-4 ">{Clientes.Endereço}</td>
-            <td className="px-6 py-4">{Clientes.Cliente}</td>
+            <td className="px-6 py-4 ">{clientes.Endereço}</td>
+            <td className="px-6 py-4">{clientes.Cliente}</td>
             <td className="px-6 py-4">{Clientes.DataFim}</td>
             <td className="px-6 py-4">{Clientes.ValorCobertura}</td>
             <td className="px-6 py-4">{Clientes.ValorFranquia}</td>
             <td className="px-6 py-4">{Clientes.QtdSinistro}</td>
             <td className="px-6 py-4 text-right">
-              <NavLink to="/ClientesCreateForm/{clientes.id}"
-                className=" font-medium text-blue-600 dark:text-blue-500 p-1 hover:underline"
-              >Editar</NavLink>
-              &nbsp;
-              <a
-                href="#"
-                className=" font-medium text-red-600 dark:text-red-500 hover:underline"
-              >
-                Excluir
-              </a>
+            <NavLink
+                  to="/clienteCreateForm"
+                  className="font-medium p-1 text-blue-600 dark:text-blue-500 hover:underline"
+                >
+                  Editar
+                </NavLink>
+                &nbsp;
+                <a
+                  href="#"
+                  className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Excluir
+                </a>
             </td>
           </tr>
-        ))}
+       
             
           </tbody>
         </table>
